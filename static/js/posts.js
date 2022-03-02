6377d2ec9664a806eaf9b3f09b41f5ebf4493751
Norhan El shaer <norhanelshaer@Norhans-Air.fios-router.home>
2022-03-02 18:29:12 -0500

    updated

---
+++ static/js/posts.js
@@ -0,0 +1,13 @@
+///////////////////////////
+//JavaScript for posts page
+///////////////////////////
+
+$(function () {
+  // Executed when js-menu-icon is clicked
+  $(".js-menu-icon").click(function () {
+    //$(this): self element, namely div.js-menu-icon
+    //next(): next to div.js-menu-icon, namely div.menu
+    //toggle(): Switch show and hide
+    $(this).next().toggle();
+  });
+});
